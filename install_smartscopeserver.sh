cce1d883adac77fd4ae821a18fe565fd8f8d9072
schreinerman <manuel@schreiner-home.info>
2019-07-28 21:07:39 +0200

    Updated missing bash variables in smartscope.daemon

--- install_smartscopeserver.sh
+++ install_smartscopeserver.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 apt-get update
-apt-get install git libusb-dev libusb-1.0-0-dev libusb-1.0-0 libavahi-common-dev libavahi-client-dev 
+apt-get install git libusb-dev libusb-1.0-0-dev libusb-1.0-0 libavahi-common-dev avahi-daemon
+
 git clone https://github.com/labnation/DeviceInterface.CXX
 cd DeviceInterface.CXX
 make LEDE=0
@@ -28,23 +29,23 @@ cat << EOF >/usr/local/bin/smartscope.daemon
 Logfile=/var/log/smartscope
 #
 LostCount=1
-rm $Logfile
+rm \$Logfile
 while true
 do
    #Start smartscopeserver process
-   /usr/bin/smartscopeserver >> $Logfile 2>&1 &
-   PID=$!
-   echo Pid is $PID >>$Logfile
+   /usr/bin/smartscopeserver >> \$Logfile 2>&1 &
+   PID=\$!
+   echo Pid is \$PID >>\$Logfile
    #Check for connection problems in logfile and restart
-   while [ $(grep -s -c "Moving from state Started -> Stopped" $Logfile) -lt $LostCount ]
+   while [ \$(grep -s -c "Moving from state Started -> Stopped" \$Logfile) -lt \$LostCount ]
    do
      /bin/sleep 2
    done
    #We have a lost connection
-   echo Found lost connection >>$Logfile
-   echo Killing SmartScope process $PID >>$Logfile
-   /bin/kill $PID
-   echo This is the $LostCount restart >>$Logfile
+   echo Found lost connection >>\$Logfile
+   echo Killing SmartScope process \$PID >>\$Logfile
+   /bin/kill \$PID
+   echo This is the \$LostCount restart >>\$Logfile
    # Lostcount is used because multiple connection failures can be in logfile.
    (( LostCount += 1 ))
    /bin/sleep 2
@@ -56,3 +57,4 @@ chmod 755  /usr/local/bin/smartscope.daemon
 systemctl enable smartscopeserver.service
 systemctl daemon-reload 
 systemctl start smartscopeserver
+exit
